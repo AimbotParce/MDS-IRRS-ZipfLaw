8f1d546237bc511a946c304c7a3e569e7d19be49
AimbotParce <marcparcerisa@gmail.com>
2025-09-17 17:18:01 +0200

    Add preprocess stage

--- zipf_law/scripts/preprocess.py
+++ zipf_law/scripts/preprocess.py
@@ -1,9 +1,9 @@
+import pprint
+
 import nltk
 
 
-def preprocess_file(file_path: str):
-    with open(file_path, "r", encoding="utf-8") as file:
-        text = file.read()
+def preprocess_file(text: str):
 
     # Tokenize the text into words
     tokens = nltk.word_tokenize(text)
@@ -20,10 +20,29 @@ def preprocess_file(file_path: str):
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) != 2:
-        print("Usage: python preprocess.py <file_path>")
+    if len(sys.argv) != 3:
+        print("Usage: python preprocess.py <file_path> <output_path>")
         sys.exit(1)
 
     file_path = sys.argv[1]
-    words = preprocess_file(file_path)
-    print(words)
+    output_path = sys.argv[2]
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        text = file.read()
+    words = preprocess_file(text)
+
+    if not words:
+        raise ValueError("No words found after preprocessing.")
+
+    with open(output_path, "w", encoding="utf-8") as out_file:
+        words_iter = iter(words)
+        while True:
+            line = next(words_iter, None)
+            if line is None:
+                break
+            while len(line) < 1000:
+                next_word = next(words_iter, None)
+                if next_word is None:
+                    break
+                line += " " + next_word
+            out_file.write(line + "\n")
